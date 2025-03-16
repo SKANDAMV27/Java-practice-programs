cee1ec3efef15856e5ff087bad9b2be78f130f67
Skanda <skandagowda0@gmail.com>
2025-03-16 20:21:43 +0530

    Simple code

---
+++ Addition.java
@@ -0,0 +1,22 @@
+class Addition
+{
+    public static void main(String[] args)
+    {
+        int add1=120;
+        int add2=140;
+        int result = add1+add2;
+        System.out.println("Additional Operantion: "+result);
+        System.out.println("End Additional Operantion");
+        int sub1=45;
+        int sub2=20;
+        int result1=sub1-sub2;
+        System.out.println("Substraction Operantion: "+result1);
+        System.out.println("End Substraction Operation");
+        int mul1=40;
+        int mul2=2;
+        int result2=mul1*mul2;
+        System.out.println("Multipliction Operation: "+result2);
+        System.out.println("End Opernation!");
+
+    }
+}
